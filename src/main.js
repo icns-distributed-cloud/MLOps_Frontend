bcafef68d0f0694beefae5842303aac246207425
jo1132 <jhk1132@gmail.com>
2023-01-17 05:50:39 +0000

    23-01-17 163

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
-//axios.defaults.baseURL = "http://163.180.117.186:18088";
-axios.defaults.baseURL = "http://210.114.91.98:18088";
+axios.defaults.baseURL = "http://163.180.117.186:18088";
+//axios.defaults.baseURL = "http://210.114.91.98:18088";
 new Vue({
   router,
   store,
